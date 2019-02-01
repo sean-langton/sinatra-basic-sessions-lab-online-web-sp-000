dcf00d53667af324b1aa0d82d477281124fc53f4
Sean Langton <slangton1@gmail.com>
2019-02-01 21:26:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,5 +7,9 @@ class App < Sinatra::Base
     set :session_secret, "87827cb2ab34ec45aff97fb396dcfe34e6be4a2564c8047ffacd3fa8e519f0cf19a277feefc59a45c02594ac51d83362c1921a38168575ea8fb65c3d8485a232"
   end
 
-  
+  get "/" do
+    erb :index
+  end
+
+
 end
